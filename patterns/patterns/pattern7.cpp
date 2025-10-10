361f311309b199b8988507e42d5a36642d6dd6c3
sheshmani09 <sheshmanisahu09@gmail.com>
2025-10-10 23:36:45 +0530

    pattern 7

--- patterns/patterns/pattern7.cpp
+++ patterns/patterns/pattern7.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
-    
+    for(int i=0;i<5;i++){
+        for (int j = 0; j <=5-i-1 ; j++)
+        {
+            cout<<" ";
+        }
+        for (int k = 0; k < 2*i+1; k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
     return 0;
 }
