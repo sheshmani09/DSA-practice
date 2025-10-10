89034054ad03d3b4e9a2316a8c783a39dff1fe0b
sheshmani09 <sheshmanisahu09@gmail.com>
2025-10-11 00:11:35 +0530

    pattern 7 and 8

--- patterns/patterns/pattern8.cpp
+++ patterns/patterns/pattern8.cpp
@@ -1,17 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    for(int i=0;i<5;i++){
-        for (int j = 0; j <=5-i-1 ; j++)
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i; j++)
         {
             cout<<" ";
         }
-        for (int k = 0; k < 2*i+1; k++)
+        for (int k = 0; k < 2*(5-i)-1; k++)
         {
             cout<<"*";
         }
         cout<<endl;
     }
+    
     return 0;
 }
