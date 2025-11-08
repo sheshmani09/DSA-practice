6bceacc40eed4d2bdb1193c31063a628b4ccd6f5
sheshmani09 <sheshmanisahu09@gmail.com>
2025-11-08 17:14:28 +0530

    selection sort

---
+++ sortingAlgo/selection.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int mini = i;
+        for (int j = i + 1; j <= n - 1; j++)
+        {
+            if (arr[j] < arr[mini])
+                mini = j;
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+    cout<<"sorted array: "<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int n;
+    cout<<"enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout<<"enter elements: "<<endl;
+    for (int i = 0; i < n; i++)
+    { 
+        cin >> arr[i];
+    }
+
+    selection_sort(arr, n);
+
+    return 0;
+}
