c860b41dbfc952780ce5870846ca25eadf7aa23a
sheshmani09 <sheshmanisahu09@gmail.com>
2025-11-10 13:18:50 +0530

    bubble

--- sortingAlgo/bubble.cpp
+++ sortingAlgo/bubble.cpp
@@ -1,27 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bubble_sort(vector <int> &arr, int n){
-    for (int i = n-1; i >=1; i--)
+void bubble_sort(vector<int> &arr, int n)
+{
+    for (int i = n - 1; i >= 1; i--)
     {
-       for(int j=0;j<i;j++){
-        if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
-       }
+        bool swapped = false;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
     }
-    
+
     for (int i = 0; i < n; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
-    
 }
-int main() {
+int main()
+{
     int n;
-    cin>>n;
-    vector <int> arr(n);
-    for(int i=0;i<n;i++) cin>>arr[i];
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
-    bubble_sort(arr,n);
+    bubble_sort(arr, n);
 
     return 0;
 }
