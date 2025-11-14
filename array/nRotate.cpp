a919d9b81a5f9fc0d22410796715d203ce7dfbe3
sheshmani09 <sheshmanisahu09@gmail.com>
2025-11-14 17:34:29 +0530

    optimal solution of left rotation

--- array/nRotate.cpp
+++ array/nRotate.cpp
@@ -2,22 +2,10 @@
 using namespace std;
 void rotate(vector<int> &arr, int n, int k)
 {
-    k =k%n;
-    vector<int> temp(k);
-    for (int i = 0; i < k; i++)
-    {
-        temp[i] = arr[i];
-    }
-    for (int i = k; i < n; i++)
-    {
-        arr[i - k] = arr[i];
-    }
-    for (int i = 0; i < k; i++)
-    {
-        arr[n-k+i]=temp[i];
-    }
-    
-
+    k = k % n;
+    reverse(arr.begin(), arr.begin() + k); 
+    reverse(arr.begin() + k, arr.end());   
+    reverse(arr.begin(), arr.end());
     cout << "rotated array: " << endl;
     for (int i = 0; i < n; i++)
     {
@@ -37,3 +25,17 @@ int main()
     rotate(arr, n, k);
     return 0;
 }
+
+// brute force ********** vector<int> temp(k);
+//  for (int i = 0; i < k; i++)
+//  {
+//      temp[i] = arr[i]; //temp.push_back(arr[i])
+//  }
+//  for (int i = k; i < n; i++)
+//  {
+//      arr[i - k] = arr[i];
+//  }
+//  for (int i = 0; i < k; i++)
+//  {
+//      arr[n-k+i]=temp[i];
+//  }
